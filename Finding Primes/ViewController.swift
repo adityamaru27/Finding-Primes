d1d9c1bffe42b3f0c853381d6eda66f221d70871
adityamaru27 <a2maru@uwaterloo.ca>
2016-05-04 23:23:15 -0400

    PrimeGenerator

--- Finding Primes/ViewController.swift
+++ Finding Primes/ViewController.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var screenView: UILabel!
+    @IBOutlet weak var entry: UITextField!
+    
+    @IBAction func buttonInput(sender: AnyObject)
+    {
+        var counter = 2;
+        var isprime = true;
+        if let number = Int(entry.text!)
+        {
+            if number == 1
+            {
+                screenView.text = "Neither prime nor composite!"
+            }
+            else if number == 2
+            {
+                screenView.text = "That is a prime!"
+            }
+            else
+            {
+                while(counter < number)
+                {
+                    if number % counter == 0
+                    {
+                        isprime = false;
+                        break;
+                    }
+                    else
+                    {
+                        isprime = true;
+                        ++counter;
+                    }
+                }
+            
+            }
+        
+            if isprime
+            {
+                screenView.text = "That is prime!"
+            }
+            else
+            {
+                screenView.text = "That is not a prime!"
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
